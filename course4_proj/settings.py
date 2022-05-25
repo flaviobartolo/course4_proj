749906dbbeef203afd92b2a0ea442fd0d7e42f54
Flavio Bartolo <flavio.bartolo2@gmail.com>
2022-05-25 14:55:23 +0000

    Finish celery introduction

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -49,7 +49,8 @@ class Dev(Configuration):
         'django.contrib.messages',
         'django.contrib.staticfiles',
         'movies',
-        'gh'
+        'gh',
+        'django_celery_results'
     ]
 
     MIDDLEWARE = [
@@ -160,3 +161,6 @@ class Dev(Configuration):
     }
 
     OMDB_KEY = "dbd7e38"
+
+    CELERY_RESULT_BACKEND = "django-db"
+    CELERY_BROKER_URL = "redis://localhost:6379/0"
